c252e28ea5229c3cca09e065260832c96b00193d
James Huang <j3505151@gmail.com>
2022-05-18 11:53:32 +0100

    Update and rename title_and_subtitle.ipynb to title_and_subtitle.py

---
+++ title_and_subtitle.py
@@ -0,0 +1,6 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+ax.update_layout('TITLE' + 
+                 '<br>' + 
+                 '<span style="font-size: 12px;"> SUBTITLE </span>')
