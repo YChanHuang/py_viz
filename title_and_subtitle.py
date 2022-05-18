096a07820e0877b6d01a428dbe02c5f0a97b24cf
James Huang <j3505151@gmail.com>
2022-05-18 11:56:37 +0100

    Update title_and_subtitle.py

--- title_and_subtitle.py
+++ title_and_subtitle.py
@@ -1,6 +1,6 @@
-import seaborn as sns
-import matplotlib.pyplot as plt
 
-ax.update_layout('TITLE' + 
+import matplotlib.pyplot as plt
+# works with plt
+plt.update_layout('TITLE' + 
                  '<br>' + 
                  '<span style="font-size: 12px;"> SUBTITLE </span>')
